d2b5ae62d2b8fc9a9ffbc1d11dbc9245ce0f5d3d
SimoneLollini <lollinisimone@gmail.com>
2022-12-23 15:53:05 +0100

    ad show

--- resources/views/admin/comics/show.blade.php
+++ resources/views/admin/comics/show.blade.php
@@ -1,5 +1,21 @@
 @extends('layouts.app')
 
 @section('contents')
-show
+<section class="show">
+    <div class="details">Comic: {{$comic->id}}</div>
+    <div class="details"><img src="{{$comic->image}}" alt=""></div>
+    <h2 class="details">{{$comic->title}}</h2>
+    <div class="details">{{$comic->price}}</div>
+    <div class="details">{{$comic->series}}</div>
+    <div class="details">{{$comic->sale_date}}</div>
+    <div class="details">{{$comic->type}}</div>
+
+
+
+
+
+
+
+
+</section>
 @endsection
