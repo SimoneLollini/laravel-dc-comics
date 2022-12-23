48d9798e6be2345199522ed555a80fed95d59b7c
SimoneLollini <lollinisimone@gmail.com>
2022-12-23 15:29:35 +0100

    Add markup in asset.index

--- resources/views/admin/comics/index.blade.php
+++ resources/views/admin/comics/index.blade.php
@@ -1,35 +1,35 @@
 @extends ('layouts.app')
 
 @section('contents')
-<div class="table-responsive">
-    <table class="table table-primary">
-        <thead>
-            <tr>
-                <th scope="col">Title</th>
-                <th scope="col">Description</th>
-                <th scope="col">Image</th>
-                <th scope="col">Price</th>
-                <th scope="col">Series</th>
-                <th scope="col">Sale_date</th>
-                <th scope="col">Type</th>
-            </tr>
-        </thead>
-        <tbody>
-            @forelse ($comics as $comic)
-            <tr class="">
-                <td scope="row">{{$comic->title}}</td>
-                <td>{{$comic->description}}</td>
-                <td>{{$comic->image}}</td>
-                <td>{{$comic->price}}</td>
-                <td>{{$comic->series}}</td>
-                <td>{{$comic->sale_date}}</td>
-                <td>{{$comic->type}}</td>
-            </tr>
-            @empty
-            <p>Ancora nessun fumetto!</p>
-            @endforelse
-        </tbody>
-    </table>
+<div class="container">
+    <div class="table-responsive">
+        <table class="table table-primary">
+            <thead>
+                <tr>
+                    <th scope="col">Title</th>
+                    <th scope="col">Image</th>
+                    <th scope="col">Price</th>
+                    <th scope="col">Series</th>
+                    <th scope="col">Sale_date</th>
+                    <th scope="col">Type</th>
+                </tr>
+            </thead>
+            <tbody>
+                @forelse ($comics as $comic)
+                <tr class="">
+                    <td scope="row">{{$comic->title}}</td>
+                    <td><img src="{{$comic->image}}" alt=""></td>
+                    <td>{{$comic->price}}</td>
+                    <td>{{$comic->series}}</td>
+                    <td>{{$comic->sale_date}}</td>
+                    <td>{{$comic->type}}</td>
+                </tr>
+                @empty
+                <p>Ancora nessun fumetto!</p>
+                @endforelse
+            </tbody>
+        </table>
+    </div>
 </div>
 
 @endsection
